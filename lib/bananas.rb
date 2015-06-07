5770d20ec5e23bf09d0ec33a219b3aa21d1071c1
nitroo <brysoncmyers@gmail.com>
2015-06-07 07:22:44 -0400

    use #include? over #match to reduce allocations

--- lib/bananas.rb
+++ lib/bananas.rb
@@ -1,7 +1,7 @@
 require "bananas/version"
 
 module Bananas
-  GEMS = `gem list`
+  GEMS = Gem::Specification.to_a.map(&:name).join
   private_constant :GEMS
 
   module_function def monkey_patches(mod = Object)
@@ -10,8 +10,8 @@ module Bananas
         .keep_if {|c| c.respond_to?(:instance_methods) }
         .map {|c| Hash[object: c, methods: c.instance_methods(false)
         .map {|m| Hash[name: m, location: (Array(c.instance_method(m).source_location).first || next)] }.compact] }
-        .map {|h| h[:methods].keep_if {|m| m[:location].match(ENV["GEM_HOME"]) }; h }
-        .map {|h| h[:methods].reject! {|m| GEMS.match(/#{h[:object].instance_method(m[:name]).owner.to_s.split("::").first}/i)}; h }
+        .map {|h| h[:methods].keep_if {|m| m[:location].include? ENV["GEM_HOME"] }; h }
+        .map {|h| h[:methods].reject! {|m| GEMS.include? h[:object].instance_method(m[:name]).owner.to_s.downcase.split("::").first}; h }
         .reject {|h| h[:methods].empty? }
         .tap {|c| puts "Couldn't find any monkey patches for module '#{mod}'" if c.empty?}
         .each {|c| puts "%{object}:" % c; c[:methods].each {|m| puts "\t%-20{name} %{location}" % m }; putc "\n" }
